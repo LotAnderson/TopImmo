1fad2a5ab3b01dbbe4814895d45448981b1279ca
LotAnderson <86662048+LotAnderson@users.noreply.github.com>
2026-06-30 12:17:06 +0200

    creating backend base

---
+++ Backend/ImmoScoutRapidApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.Sqlite;
+
+var connectionString = "Data Source=data.db";
+using var connection = new SqliteConnection(connectionString);
+connection.Open();
+
+var createTableCmd = connection.CreateCommand();
+createTableCmd.CommandText = @"
+CREATE TABLE IF NOT EXISTS JsonData (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Body TEXT NOT NULL
+);";
+createTableCmd.ExecuteNonQuery();
+
+Console.WriteLine("Table created (if it did not exist).");
